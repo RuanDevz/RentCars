35819172550f25c58d1039728b642349933cc59f
RuanDevz <Ruanbatista1509@outlook.com>
2024-05-26 21:26:49 -0300

    loading component fetching cars

--- FRONTEND/src/components/ComponentLogado/MeusCarros/MeusCarros.jsx
+++ FRONTEND/src/components/ComponentLogado/MeusCarros/MeusCarros.jsx
@@ -7,21 +7,24 @@ import Passageiro from './Images/Passageiro.png'
 import Portas from './Images/Portas.png'
 import Marcha from './Images/Marcha.png'
 import Arcondicionado from './Images/Arcondicionado.png'
+import LoadingComponent from '../../LoadingComponent/LoadingComponent'
 
 const MeusCarros = () => {
-    const { myid } = useContext(Context)
+    const { myid, setLoading, loading } = useContext(Context)
     const [mycars, setMycars] = useState([])
 
     useEffect(() => {
+        setLoading(true)
         axios.get(`https://rent-cars-jdua.vercel.app/user/${myid}/cars`)
             .then((response) => {
                 setMycars(response.data)
-                console.log(response.data)
+                setLoading(false)
             })
             .catch((error) => {
                 console.error("There was an error fetching the cars!", error)
+                setLoading(false)
             })
-    }, [myid]) 
+    }, [myid, setLoading]) 
 
     return (
         <div>
@@ -33,45 +36,50 @@ const MeusCarros = () => {
                     </p>
                 </section>
                 <section className='flex justify-around gap-10 mx-auto flex-wrap max-w-screen-xl'>
-                    {mycars.length > 0 ? (
-                        mycars.map((car) => (
-                            <div key={car.id} className='max-w-64 font-primary rounded-lg shadow-2xl p-4 mb-4'>
-                                <img className='pb-5 w-80' src={car.img} alt={car.name} />
-                                <h1 className='font-medium text-lg'>{car.name}</h1>
-                                <div className='flex gap-2 flex-wrap items-end gap-5 border-b border-gray-300'>
-                                  <article className='flex justify-center items-center'>
-                                  <div className='flex py-1'>
-                                        <img src={Passageiro} alt="Passageiro" />
-                                        <p className='text-xs text-gray-500'>{car.passageiros} Passagers</p>
-                                    </div>
-                                    <div className='flex gap-2 py-1'>
-                                        <img src={Marcha} alt="Marcha" />
-                                        <p className='text-xs text-gray-500'>{car.marcha}</p>
-                                    </div>
-                                  </article>
-                                    <article className='flex justify-center items-center'>
-                                    <div className='flex gap-2 py-1 pb-6'>
-                                        <img src={Arcondicionado} alt="Arcondicionado" />
-                                        <p className='text-xs text-gray-500'>{car.arcondicionado}</p>
-                                    </div>
-                                    <div className='flex gap-2 py-1 pb-6'>
-                                        <img src={Portas} alt="Portas" />
-                                        <p className='text-xs text-gray-500'>{car.portas} Doors</p>
-                                    </div>
-                                    </article>
-                                </div>
-                                <div className='flex justify-between pt-5 font-primary'>
-                                    <p>Price</p>
-                                    <p><strong>${car.price}</strong> <span className='text-gray-500'>/day</span></p>
-                                </div>
-                                <div className='flex justify-around items-center mt-6 font-primary'>
-                                  <button className='bg-blue-600 text-white rounded p-3'>Editar</button>
-                                  <button className='bg-red-600 text-white rounded p-3'>Remover</button>
-                                </div>
-                            </div>
-                        ))
+                    {loading ? (
+                        <div className='flex flex-col justify-center items-center h-screen'>
+                            <LoadingComponent />
+                            <p className='text-2xl font-primary text-primary font-medium mt-10'>Buscando carros...</p>
+                        </div>
                     ) : (
-                        <p>Nenhum carro encontrado.</p>
+                        mycars.length > 0 ? (
+                            mycars.map((car) => (
+                                <div key={car.id} className='max-w-64 font-primary rounded-lg shadow-2xl p-4 mb-4'>
+                                    <img className='pb-5 w-80' src={car.img} alt={car.name} />
+                                    <h1 className='font-medium text-lg'>{car.name}</h1>
+                                    <div className='flex gap-2 flex-wrap items-end gap-5 border-b border-gray-300'>
+                                        <div className='flex py-1'>
+                                            <img src={Passageiro} alt="Passageiro" />
+                                            <p className='text-xs text-gray-500'>{car.passageiros} Passagers</p>
+                                        </div>
+                                        <div className='flex gap-2 py-1'>
+                                            <img src={Marcha} alt="Marcha" />
+                                            <p className='text-xs text-gray-500'>{car.marcha}</p>
+                                        </div>
+                                        <div className='flex gap-2 py-1 pb-6'>
+                                            <img src={Arcondicionado} alt="Arcondicionado" />
+                                            <p className='text-xs text-gray-500'>{car.arcondicionado}</p>
+                                        </div>
+                                        <div className='flex gap-2 py-1 pb-6'>
+                                            <img src={Portas} alt="Portas" />
+                                            <p className='text-xs text-gray-500'>{car.portas} Doors</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex justify-between pt-5 font-primary'>
+                                        <p>Price</p>
+                                        <p><strong>${car.price}</strong> <span className='text-gray-500'>/day</span></p>
+                                    </div>
+                                    <div className='flex justify-around items-center mt-6 font-primary'>
+                                        <button className='bg-blue-600 text-white rounded p-3'>Editar</button>
+                                        <button className='bg-red-600 text-white rounded p-3'>Remover</button>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div className='bg-red-100 my-20'>
+                                <p className='text-2xl font-bold text-center text-red-600 py-5 px-5 rounded-xl'>Nenhum carro encontrado.</p>
+                            </div>
+                        )
                     )}
                 </section>
             </main>
